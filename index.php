532e8d547a7f0ad863544bcd67164c08bf516b86
jebusx33 <gabrielj82@gmail.com>
2019-09-15 10:01:40 -0300

    Web login html-css

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,17 +9,4 @@
 <body>
     <h1>test github</h1>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>test github</h1>
-</body>
->>>>>>> 83452e9f3ce3187a1cb845f91f077809edf76b81
 </html>
